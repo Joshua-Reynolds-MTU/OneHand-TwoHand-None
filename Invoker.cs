fb2a81959d876328a3b645f9cfd6c22b00a5055c
Joshua Reynolds <71860187+Joshua-Reynolds-MTU@users.noreply.github.com>
2022-01-28 18:10:40 -0500

    Add files via upload

---
+++ Invoker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Invoker : MonoBehaviour
+{
+    private NewSceneSwitcher scene1;
+
+    void Start ()
+    {
+        Invoke("sceneGrabber", 5f);
+    }
+
+    void sceneGrabber()
+    {
+        scene1 = GameObject.Find("GameManager").GetComponent<NewSceneSwitcher>();
+        scene1.invokeSwitcher();
+    }
+}
